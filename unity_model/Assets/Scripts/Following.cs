ac12db9311a6e1f457a81f6adf8ee6a9908d52be
水野淳 <>
2018-10-16 10:30:18 +0900

    特定モデルを追随する処理を追加(エラーなどありまだ不安定)

---
+++ unity_model/Assets/Scripts/Following.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Following : MonoBehaviour {
+
+    [SerializeField]
+    public Transform _unityChansTransform;
+
+    private NavMeshAgent _navMeshAgent;
+
+    [SerializeField]
+    public Animator _animator;
+
+    /// <summary>
+    /// 開始時
+    /// </summary>
+	void Start () {
+        if (!_navMeshAgent) {
+            _navMeshAgent = this.GetComponent<NavMeshAgent>();
+        }
+
+        if (!_animator) {
+            _animator = this.GetComponent<Animator>();
+        }
+	}
+	
+    /// <summary>
+    /// 更新時
+    /// </summary>
+	void Update () {
+        if (!_unityChansTransform) {
+            return;
+        }
+
+        // 目的地と自分の位置との距離
+        Vector3 dir = _unityChansTransform.transform.position - this.transform.position;
+
+        // 目的地の位置
+        Vector3 pos = this.transform.position + dir * 1.5f;
+
+        // 目的地の方を向く
+        this.transform.rotation = Quaternion.LookRotation(dir);
+
+        // 目的地を指定する
+        _navMeshAgent.destination = pos;
+
+        // 目的地からどれくらい離れて停止するか
+        _navMeshAgent.stoppingDistance = 3f;
+
+        // Agentの速度の二乗の数値でアニメーションを切り替える
+        _animator.SetFloat("Speed", _navMeshAgent.velocity.sqrMagnitude);
+    }
+}
